1ad248c319b171bb7931de51d1efc00e54287db2
Benjamin E Boson <Benjamineboson@gmail.com>
2020-09-29 21:57:52 +0200

    Added welcome screen and start of game setup + simple CSS

--- src/index.js
+++ src/index.js
@@ -2,33 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Ben extends React.Component {
-	constructor(props){
+class Game extends React.Component{
+	constructor(){
 		super();
+		this.state = {
+			numberOfRounds : 0,
+			playerName : ''
+		}
+		this.startGame = this.startGame.bind(this);
 	}
 
 	render(){
-		return(
-		<p>{this.props.value}</p>
-		)
+		if (this.state.playerName.length > 1 && this.state.numberOfRounds > 0){
+			return(
+				<div>
+					<div>
+						<h1>Player: {this.state.playerName}</h1>
+						<h1>Number of rounds: {this.state.numberOfRounds}</h1>
+					</div>
+				</div>
+			)
+		}else{
+			return(
+				<div>
+					<div className="rubric">
+						<h1>Rock Paper Scissors</h1>
+					</div>
+					<div className="setupForm">
+						<form onSubmit={this.startGame}>
+							<input type="number" name="roundsInput" placeholder="Enter amount of rounds"/>
+							<input type="text" name="userNameInput" placeholder="Enter your username"/>
+							<input type="submit" value="Start Game"/>
+						</form>
+					</div>
+				</div>
+			)
+		}
+		
 	}
-}
-class Oscar extends React.Component {
-    
-    constructor(props){
-        super()
-    }
 
-    render(){
-        return (
-            <p>{this.props.name}´s {this.props.type}</p>
-        );
-    }
+	startGame(event){
+		event.preventDefault();
+		this.setState({
+			numberOfRounds: event.target.roundsInput.value,
+			playerName: event.target.userNameInput.value,
+		})
+	}
 }
 
 
 ReactDOM.render(
-    <Oscar name="Oscar" type="test" />,
-    <Ben value="Ben"/>,
+	<div>
+		<Game/>
+	</div>,
     document.getElementById('root')
 );
