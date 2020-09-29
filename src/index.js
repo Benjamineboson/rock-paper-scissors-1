691daa92181e805b94cd51abc1ee42fe35fc12b2
Benjamin E Boson <Benjamineboson@gmail.com>
2020-09-29 15:41:24 +0200

    First commit - Ben

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Ben extends React.Component {
+	constructor(props){
+		super();
+	}
+
+	render(){
+		return(
+		<p>{this.props.value}</p>
+		)
+	}
+}
 
 ReactDOM.render(
-
+  <Ben value="Ben"/>,
   document.getElementById('root')
 );
